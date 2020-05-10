3bb561b43cd6f6e6fa3b58c8b4b19e2bbf91428e
Jesse <guitarerman@gmail.com>
2020-05-10 01:21:05 -0500

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,6 @@ int main() {
   // code, depending on the environment in which this C code is compiled).
   // By default, this would cause an infinite loop upon exception, which is
   // also "safe" behavior and the debugger can connect.
-  __asm__ volatile("csrw mtvec, %0" ::"i"(0x0));
+  __asm__ volatile("csrwi mtvec, 0");
   return 0;
 }
