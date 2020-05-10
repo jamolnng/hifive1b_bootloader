f71836dbfb025081efb39c7e794292bf026dcd3e
Jesse <guitarerman@gmail.com>
2020-05-10 01:18:59 -0500

    update

--- main.c
+++ main.c
@@ -1,1 +1,8 @@
-int main() { return 0; }
+int main() {
+  // Restore the default mtvec (which may have been set by initialization
+  // code, depending on the environment in which this C code is compiled).
+  // By default, this would cause an infinite loop upon exception, which is
+  // also "safe" behavior and the debugger can connect.
+  __asm__ volatile("csrw mtvec, %0" ::"i"(0x0));
+  return 0;
+}
