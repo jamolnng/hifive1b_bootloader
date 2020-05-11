26394a262b1a9d22e972e7ccc107838d159dde23
Jesse <guitarerman@gmail.com>
2020-05-10 21:25:27 -0500

    finish double tap loop

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 #define GPIO_OUTPUT_EN 0x08
 #define GPIO_OUTPUT_VAL 0x0C
 #define GPIO_LOW_IP 0x34
-#define GPIO_REG_IOF_EN 0x38
+#define GPIO_IOF_EN 0x38
 #define GPIO_OUT_XOR 0x40
 
 #define CLINT_CTRL_ADDR 0x02000000ul
@@ -22,6 +22,8 @@
 #define mmio(reg, offset) (*(volatile uint32_t *)((reg) + (offset)))
 
 int main() {
+  uint32_t mtime_lo, mtime_hi, next_lo, next_hi;
+
   // Restore the default mtvec (which may have been set by initialization
   // code, depending on the environment in which this C code is compiled).
   // By default, this would cause an infinite loop upon exception, which is
@@ -32,53 +34,82 @@ int main() {
   if (mmio(AON_CTRL_ADDR, AON_BACKUP15) == BACKUP15_MAGIC) {
     // 200003ec
     // Reset was "double-tapped".
-  } else {
-    // 2000025a
-    // gpio
-    // enable and invert output for pin 19 (green led)
-    mmio(GPIO_CTRL_ADDR, GPIO_OUTPUT_EN) |= GREEN_LED;
-    mmio(GPIO_CTRL_ADDR, GPIO_OUT_XOR) |= GREEN_LED;
+    mmio(AON_CTRL_ADDR, AON_BACKUP15) = 0;
 
-    // disable gpio function on pin 19
-    mmio(GPIO_CTRL_ADDR, GPIO_REG_IOF_EN) |= ~GREEN_LED;
+    mmio(GPIO_CTRL_ADDR, GPIO_OUTPUT_EN) |= RED_LED;
+    mmio(GPIO_CTRL_ADDR, GPIO_OUT_XOR) |= RED_LED;
+    mmio(GPIO_CTRL_ADDR, GPIO_IOF_EN) &= ~RED_LED;
+    mmio(GPIO_CTRL_ADDR, GPIO_OUTPUT_VAL) |= RED_LED;
 
-    // toggle green led
-    mmio(GPIO_CTRL_ADDR, GPIO_OUTPUT_VAL) |= GREEN_LED;
+    mmio(GPIO_CTRL_ADDR, GPIO_OUTPUT_EN) &= ~GREEN_LED;
 
-    // disable pin 22 (red led)
-    mmio(GPIO_CTRL_ADDR, GPIO_OUTPUT_EN) &= ~RED_LED;
-
-    // disable gpio function on pin 22 (red led)
-    mmio(GPIO_CTRL_ADDR, GPIO_REG_IOF_EN) |= ~RED_LED;
-
-    // aon
-    // 20000298: could be useless
-    // uint32_t aon_backup15 = mmio(AON_CTRL_ADDR, AON_BACKUP15);
-    // 2000029c
-    mmio(AON_CTRL_ADDR, AON_BACKUP15) = BACKUP15_MAGIC;
-
-    // clint
-    uint32_t mtime_lo = mmio(CLINT_CTRL_ADDR, CLINT_MTIME);
-    uint32_t mtime_hi = mmio(CLINT_CTRL_ADDR, CLINT_MTIME + 4);
-    uint32_t next_mtime_lo = mtime_lo + 0x4000;
-    // overflow, add 1 to mtime_hi
-    uint32_t next_mtime_hi = mtime_hi + ((next_mtime_lo < mtime_lo) ? 1 : 0);
-
-    mtime_lo = mmio(CLINT_CTRL_ADDR, CLINT_MTIME);
-    mtime_hi = mmio(CLINT_CTRL_ADDR, CLINT_MTIME + 4);
-    // 200002cc
-    if (mtime_hi >= next_mtime_hi) {
-      // 2000048c
-    } else {
-      // 200002d0
-      // 200002d4
-      // 200002e0
-      while (mtime_hi < next_mtime_hi) {
-        mtime_lo = mmio(CLINT_CTRL_ADDR, CLINT_MTIME);
+    do {
+      mtime_lo = mmio(CLINT_CTRL_ADDR, CLINT_MTIME);
+      mtime_hi = mmio(CLINT_CTRL_ADDR, CLINT_MTIME + 4);
+      next_lo = mtime_lo + 0x4000;
+      next_hi = mtime_hi + (next_lo < mtime_lo);
+      // 20000456
+      while (mtime_hi < next_hi) {
         mtime_hi = mmio(CLINT_CTRL_ADDR, CLINT_MTIME + 4);
       }
-    }
+
+      do {
+        mtime_lo = mmio(CLINT_CTRL_ADDR, CLINT_MTIME);
+        mtime_hi = mmio(CLINT_CTRL_ADDR, CLINT_MTIME + 4);
+      } while (next_hi == mtime_hi && mtime_lo < next_lo);
+
+      mmio(GPIO_CTRL_ADDR, GPIO_OUT_XOR) ^= RED_LED;
+
+    } while (1);
   }
-}
-return 0;
+  // 2000025a
+  // gpio
+  // enable and invert output for pin 19 (green led)
+  mmio(GPIO_CTRL_ADDR, GPIO_OUTPUT_EN) |= GREEN_LED;
+  mmio(GPIO_CTRL_ADDR, GPIO_OUT_XOR) |= GREEN_LED;
+
+  // disable gpio function on pin 19
+  mmio(GPIO_CTRL_ADDR, GPIO_IOF_EN) |= ~GREEN_LED;
+
+  // toggle green led
+  mmio(GPIO_CTRL_ADDR, GPIO_OUTPUT_VAL) |= GREEN_LED;
+
+  // disable pin 22 (red led)
+  mmio(GPIO_CTRL_ADDR, GPIO_OUTPUT_EN) &= ~RED_LED;
+
+  // disable gpio function on pin 22 (red led)
+  mmio(GPIO_CTRL_ADDR, GPIO_IOF_EN) &= ~RED_LED;
+
+  // aon
+  // 20000298: could be useless
+  // uint32_t aon_backup15 = mmio(AON_CTRL_ADDR, AON_BACKUP15);
+  // 2000029c
+  mmio(AON_CTRL_ADDR, AON_BACKUP15) = BACKUP15_MAGIC;
+
+  // clint
+  mtime_lo = mmio(CLINT_CTRL_ADDR, CLINT_MTIME);
+  mtime_hi = mmio(CLINT_CTRL_ADDR, CLINT_MTIME + 4);
+  next_lo = mtime_lo + 0x4000;
+  // overflow, add 1 to mtime_hi
+  next_hi = mtime_hi + ((next_lo < mtime_lo) ? 1 : 0);
+
+  mtime_lo = mmio(CLINT_CTRL_ADDR, CLINT_MTIME);
+  mtime_hi = mmio(CLINT_CTRL_ADDR, CLINT_MTIME + 4);
+  // 200002cc
+  if (mtime_hi >= next_hi) {
+    // 2000048c
+  } else {
+    // 200002d0
+    // 200002d4
+    // 200002e0
+    while (mtime_hi < next_hi) {
+      mtime_hi = mmio(CLINT_CTRL_ADDR, CLINT_MTIME + 4);
+    }
+
+    do {
+      mtime_lo = mmio(CLINT_CTRL_ADDR, CLINT_MTIME);
+      mtime_hi = mmio(CLINT_CTRL_ADDR, CLINT_MTIME + 4);
+    } while (mtime_hi == next_hi && mtime_lo < next_lo);
+  }
+  return 0;
 }
